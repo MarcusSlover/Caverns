b9e73d45dff5076013d45736a8d22d75e7bd8378
MarcusSlover <mariuszek016@gmail.com>
2021-04-03 00:12:22 +0200

    + utils

---
+++ src/main/java/me/marcusslover/caverns/utils/TaskUtil.java
@@ -0,0 +1,26 @@
+package me.marcusslover.caverns.utils;
+
+import me.marcusslover.caverns.Caverns;
+import org.bukkit.Bukkit;
+
+import java.util.function.Consumer;
+
+public class TaskUtil {
+    private static final Caverns CAVERNS = Caverns.getInstance();
+
+    public static void runAsync(Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(CAVERNS, runnable);
+    }
+
+    public static void runAsync(Runnable runnable, Consumer<Long> whenDone) {
+        Bukkit.getScheduler().runTaskAsynchronously(CAVERNS, () -> {
+            long currentTime = System.currentTimeMillis();
+            runnable.run();
+            runSync(() -> whenDone.accept(System.currentTimeMillis() - currentTime));
+        });
+    }
+
+    public static void runSync(Runnable runnable) {
+        Bukkit.getScheduler().runTask(CAVERNS, runnable);
+    }
+}
